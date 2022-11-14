a1a20715ece4a816da061525aef6658124b68082
Kevin <ghekghek2@gmail.com>
2022-11-14 17:08:36 +0800

    add intersection  animation in about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,63 @@
-import React from 'react'
+import React ,{useRef} from 'react'
+import {useIntersection} from 'react-use';
+
+import gsap from "gsap"
 import '../style/about.css'
 import Pin from '../images/pin.png'
+
+
 function About() {
+
+
+   const sectionRef = useRef(null)
+
+
+
+   const intersection = useIntersection(sectionRef,{
+    root:null,
+    rootMargin:"200px",
+    threshold:1
+   })
+
+const fadeIn = element =>{
+gsap.to(element,1,{
+
+  y:-60,
+
+  ease:'power4.out',
+  margin:0,
+  opacity:1,
+  stagger:{
+    amount:.5
+  }
+})
+}
+
+const fadeOut = element =>{
+  gsap.to(element,1,{
+    opacity:0,
+    y:-20,
+    ease:'power4.out',
+
+  })
+}
+
+
+intersection && intersection.intersectionRatio < 1
+?fadeOut(".fadeIn")
+:fadeIn(".fadeIn")
+
+    
+
   return (
     <div className='about section'>
 
 
- <div className="about-box section">
-            <div className='about-header'>
-   
-       <p className="title">    About Kevin</p>
-                <p>
+ <div ref={sectionRef }   className="   about-wrapper section fadeIn ">
+<div className=' about-me fadeIn '>
+       <div className="left-box   box">  
+       <p className='about-title fadeIn'> About Kevin</p>
+                <p   className=' p about-content fadeIn'>
                   I'm Kevin Divinagracia a Freelancer Computer Technician based
                   on Mariveles Bataan with years of experience of repairing,
                   installation, troubleshooting and updating softwares and
@@ -18,24 +65,23 @@ function About() {
                   the world of web development. A Career shifter that wanted to
                   explore and utilize my strong background and skill being a
                   computer technician .
-                </p>
-    <img src={Pin} alt="" />
+                  </p>
+                  <img  src={Pin} alt="" />
+                </div>
+ 
             </div>
-            <div className="about-right">
-              <div className="like">
-                <div className="like-title">
-                  <h5>Interested to</h5>
-                </div>
-                <div className="like-content">
+            <div className="right-box box ">
+                  <p className='about-title fadeIn'>Interested to</p>
+                <div className=" like-content fadeIn">
                   <ul>
-                    <li>Web Design</li>
-                    <li>Graphics Design</li>
-                    <li>Ui/Ux</li>
-                    <li>Animation</li>
-                    <li>Computer Repair</li>
-                    <li>Smartphones Repair</li>
+                    <li className=' fadeIn '><p className='p' >Web Design</p></li>
+                    <li className=' fadeIn '><p className='p' >Graphics Design</p></li>
+                    <li className=' fadeIn '><p className='p' >Ui/Ux</p></li>
+                    <li className=' fadeIn '><p className='p'>Animation</p></li>
+                    <li className=' fadeIn '><p className='p' >Computer Repair</p></li>
+                    <li className=' fadeIn '><p className='p' >Smartphones Repair</p></li>
                   </ul>
-                </div>
+            
               </div>
               <img src={Pin} alt="" />
             </div>
